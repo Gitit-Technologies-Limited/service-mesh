97a964da00f857b29d967b0962f8e40458d66398
LENOVO <ambagape@gmail.com>
2024-02-21 23:09:04 +0100

    add auth, common and tracing microservices

---
+++ auth/src/main/java/com/gitittech/auth/service/AuthService.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gitittech.auth.service;
+
+/**
+ *
+ * @author HP
+ */
+public class AuthService {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
