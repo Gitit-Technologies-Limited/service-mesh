f82bb5d787183264659e6686f6cc9131c6ff2ec9
LENOVO <ambagape@gmail.com>
2024-02-21 13:21:48 +0100

    create service mesh

---
+++ event-bus/src/main/java/com/gitittech/event/bus/EventBusApplication.java
@@ -0,0 +1,17 @@
+
+
+package com.gitittech.event.bus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author HP
+ */
+@SpringBootApplication
+public class EventBusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventBusApplication.class, args);
+    }
+}
