42b2754fe3b1c6f2854f12f434b1fbcbabddd09b
LENOVO <ambagape@gmail.com>
2024-02-23 23:16:41 +0100

    configure auth service to work with config-service and discovery service

---
+++ auth/src/main/java/com/gitittech/auth/service/AuthServiceApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gitittech.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author HP
+ */
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
