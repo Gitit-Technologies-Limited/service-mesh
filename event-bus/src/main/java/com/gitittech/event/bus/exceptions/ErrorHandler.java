4309c8fe52f3c8a17305a3ac8f60fc642d5214fd
LENOVO <ambagape@gmail.com>
2024-02-25 22:19:57 +0100

    introduce ErrorHandler to event-bus

---
+++ event-bus/src/main/java/com/gitittech/event/bus/exceptions/ErrorHandler.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gitittech.event.bus.exceptions;
+
+import com.gitittech.common.utils.ErrorUtil;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author HP
+ */
+@ControllerAdvice
+public class ErrorHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+       
+        return ErrorUtil.handleValidationExceptions(ex);
+    }
+}
