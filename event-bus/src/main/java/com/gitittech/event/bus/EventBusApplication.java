7dd73cf5a15a24a33a010f3dc746da070c4d1493
LENOVO <ambagape@gmail.com>
2024-02-26 08:06:40 +0100

    get event-bus to be stable

--- event-bus/src/main/java/com/gitittech/event/bus/EventBusApplication.java
+++ event-bus/src/main/java/com/gitittech/event/bus/EventBusApplication.java
@@ -4,12 +4,14 @@ package com.gitittech.event.bus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  *
  * @author HP
  */
 @SpringBootApplication
+@ComponentScan("com.gitittech.event.bus.services")
 public class EventBusApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventBusApplication.class, args);
--- event-bus/src/main/java/com/gitittech/event/bus/services/ProducerService.java
+++ event-bus/src/main/java/com/gitittech/event/bus/services/ProducerService.java
@@ -6,21 +6,23 @@ package com.gitittech.event.bus.services;
 
 import com.gitittech.event.bus.models.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.cloud.stream.function.StreamBridge;
 
 /**
  *
  * @author Ambrose Ariaiegbe
  */
-class ProducerService implements IProducerService{
-    
+@Service
+class ProducerService implements IProducerService {
+
     private static long id = 0l;
-    
+
     @Autowired
     private StreamBridge bridge;
-        
+
     @Override
-    public void sendEvent(Event msg){
+    public void sendEvent(Event msg) {
         msg.setId(++id);
         this.bridge.send(msg.getType(), msg);
     }
